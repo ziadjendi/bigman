c7a296a86ffc445349cff8b824e84f918245841c
Ziad <ziadjendi@gmail.com>
2020-07-17 15:52:43 +0400

    fix bugs & refactor code

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const listCollections = async () => {
 const getCollectionUID = async (collectionName) => {
   const { collections } = await pmapi.getCollections();
   const coll = collections.find((coll) => coll.name === collectionName);
+  if (!coll) throw new Error("The collection name is not valid/exists");
   return coll.uid;
 };
 
@@ -22,6 +23,7 @@ const listEnvironments = async () => {
 const getEnvironmentUID = async (environmentName) => {
   const { environments } = await pmapi.getEnvironments();
   const env = environments.find((env) => env.name === environmentName);
+  if (!env) throw new Error("The environment name is not valid/exists");
   return env.uid;
 };
 
@@ -64,21 +66,35 @@ const workflowGroups = async (collectionID) => {
   return groups;
 };
 
-const listWorkflows = async (groupName) => {
-  const collections = await listCollections();
-  let groupInfo = null;
-  for (const coll of collections) {
-    const groups = await workflowGroups(coll.uid);
-    groupInfo = groups.find((group) => group.name === groupName);
+const listWFGroups = async (collectionName) => {
+  const collectionID = await getCollectionUID(collectionName);
+  const res = await workflowGroups(collectionID);
+  const wfGroups = res.map((item) => item.name);
+  console.log(wfGroups);
+  return wfGroups;
+};
 
-    if (groupInfo) {
-      groupInfo.collectionID = coll.uid;
-      break;
+const listWorkflows = async (groupName) => {
+  try {
+    const collections = await listCollections();
+    let groupInfo = null;
+    for (const coll of collections) {
+      const groups = await workflowGroups(coll.uid);
+      groupInfo = groups.find((group) => group.name === groupName);
+
+      if (groupInfo) {
+        groupInfo.collectionID = coll.uid;
+        break;
+      }
     }
+    if (!groupInfo)
+      throw new Error("The workflow group name is not valid/exists");
+    const workflows = groupInfo.item.map((wf) => wf.name);
+    const result = { collectionID: groupInfo.collectionID, workflows };
+    return result;
+  } catch (error) {
+    console.error(error);
   }
-  const workflows = groupInfo.item.map((wf) => wf.name);
-  const result = { collectionID: groupInfo.collectionID, workflows };
-  return result;
 };
 
 const runTest = async ({
@@ -90,9 +106,9 @@ const runTest = async ({
 }) => {
   if (typeof collectionName !== "string")
     throw new Error("Collection name is not valid!!!");
+  const environmentUID = await getEnvironmentUID(environment);
   if (Object.keys(variables)) await setEnvironmentVars(environment, variables);
   const collectionUID = await getCollectionUID(collectionName);
-  const environmentUID = await getEnvironmentUID(environment);
   if (!workflows.length && !wfGroups.length) {
     test(collectionUID, environmentUID);
   } else {
@@ -118,7 +134,7 @@ const runTest = async ({
 module.exports.default = {
   listCollections,
   getCollectionUID,
-  workflowGroups,
+  listWFGroups,
   listWorkflows,
   listEnvironments,
   getEnvironmentUID,
@@ -128,7 +144,7 @@ module.exports.default = {
 
 exports.listCollections = listCollections;
 exports.getCollectionUID = getCollectionUID;
-exports.workflowGroups = workflowGroups;
+exports.listWFGroups = listWFGroups;
 exports.listWorkflows = listWorkflows;
 exports.listEnvironments = listEnvironments;
 exports.getEnvironmentUID = getEnvironmentUID;
